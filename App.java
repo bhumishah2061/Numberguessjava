0cbf13f7bd3d776d9d7a07781b87e242844ce717
bhumishah2061 <bhumishah2061@gmail.com>
2025-05-10 21:51:57 +0545

    first code added

---
+++ App.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+ 
+public class App{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+ 
+        int random = (int) (Math.random() * 100);
+        System.out.println("Please Guess a two digit number");
+ 
+        boolean flag = true;
+        int count = 0;
+ 
+        while (flag) {
+            System.out.print("Guess the number ");
+            int guess = scan.nextInt();
+ 
+            if (guess > random) {
+                System.out.println("Lower number please");
+                count++;
+            } else if (guess < random){
+                System.out.println("Higher number please");
+                count++;
+            } else{
+                flag = false;
+            }
+            
+        }
+ 
+        System.out.println("You guessed in " + count + " attempt");
+    }
+}
+ 
